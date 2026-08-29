4dfda99fcbc172be542937d01bcdf5131cd9eda9
Ajaykapratwar <kapratwarajay@gmail.com>
2026-08-29 09:39:31 +0530

    Configure Eureka server and application properties for discovery service

--- src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
+++ src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -2,8 +2,10 @@ package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer    // this make it as a eureka server
 public class DiscoveryserviceApplication {
 
 	public static void main(String[] args) {
